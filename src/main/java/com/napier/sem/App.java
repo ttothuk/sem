aa54a04d60dbe5008e89b2d4142d2fefc1b4a93c
Tibor Toth <40400971@live.napier.ac.uk>
2020-01-16 11:24:18 +0000

    Testing, testing

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -4,5 +4,7 @@ public class App {
     public static void main(String[] args)
     {
         System.out.println("Boo yah!");
+        System.out.println("Boo yah!");
+        System.out.println("Boo yah!");
     }
 }
